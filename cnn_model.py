3d32bf1eb9a0273972a0d975ce6b5784e779f7bd
the website devil <98803802+alexwelsby@users.noreply.github.com>
2025-08-22 13:53:58 +1200

    replaced dummyConcat with copy of Concat class from chainer 1.16
    Signed-off-by: the website devil <98803802+alexwelsby@users.noreply.github.com>

--- cnn_model.py
+++ cnn_model.py
@@ -7,15 +7,51 @@ import chainer.links as L
 from chainer.functions.loss import softmax_cross_entropy
 from chainer.functions.evaluation import accuracy
 from chainer import reporter
-from chainer.functions.array.concat import Concat
+from chainer.utils import type_check
+from chainer import cuda
+import numpy as np
 
-
-
+#stolen from the 1.16 release of chainer lol
 class DummyConcat:
-    def __call__(self, *xs):
-        # never actually called for real concat
-        print("Dummy concat called")
-        return xs[0]  
+    # concat along the channel dimension by default
+    def __init__(self, axis=1):
+        if not isinstance(axis, int):
+            raise TypeError('axis must be int')
+
+        self.axis = axis
+
+    def check_type_forward(self, in_types):
+        type_check.expect(in_types.size() > 0)
+        type_check.expect(in_types[0].ndim >
+                          type_check.Variable(self.axis, 'axis'))
+
+        type_check.expect(
+            -in_types[0].ndim <= self.axis,
+            self.axis < in_types[0].ndim
+        )
+        ndim = in_types[0].ndim.eval()
+        axis = self.axis % ndim
+        for i in range(1, in_types.size().eval()):
+            type_check.expect(
+                in_types[0].dtype == in_types[i].dtype,
+                in_types[0].ndim == in_types[i].ndim,
+            )
+            for d in range(0, ndim):
+                if d == axis:
+                    continue
+                type_check.expect(in_types[0].shape[d] == in_types[i].shape[d])
+
+    def forward(self, xs):
+        xp = cuda.get_array_module(*xs)
+        return xp.concatenate(xs, axis=self.axis),
+
+    def backward(self, xs, gy):
+        if not xs[:-1]:
+            return gy
+
+        xp = cuda.get_array_module(*xs)
+        sizes = np.array([x.shape[self.axis] for x in xs[:-1]]).cumsum()
+        return xp.split(gy[0], sizes, axis=self.axis)
 
 # CONV -> Batch -> ReLU
 class ConvBlock(chainer.Chain):
